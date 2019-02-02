b61273deb0e6658a6605494993a763239f7461c7
Diego Marinho <marinhop.diego@gmail.com>
2019-02-02 16:33:53 -0200

    feat: Added [makeReactComponentSkeleton] files.

---
+++ gallery.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './gallery.scss';
+
+class gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+    );
+  }
+}
+
+
+gallery.propTypes = {
+};
+gallery.defaultProps = {
+};
+
+export default gallery;
